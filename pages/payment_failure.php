73c617ee44ae089c9710b898899d3d03d59b44af
abhishek-2006 <shahabhishek409@gmail.com>
2026-03-22 23:08:56 +0530

    fix: Correct condition check for POST request in payment failure handling

--- pages/payment_failure.php
+++ pages/payment_failure.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION['payment_flow'])) {
     exit();
 }
 
-if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // 1. Collect PayU Response
     $status      = $_POST['status'];
